8d4a4a7b22eb619298cc8710730123da3f73df13
yanzacd92 <yanzacd@gmail.com>
2023-03-21 01:52:35 +0000

    Add code to generate the behaviour of the card

--- src/app.js
+++ src/app.js
@@ -9,3 +9,39 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+window.changeCard = function changeCard() {
+  const icons = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  let randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  console.log("ICON " + randomIcon);
+  console.log("NUMBER " + randomNumbers);
+
+  document.querySelector(".iconTop").innerHTML = randomIcon;
+  document.querySelector(".number").innerHTML = randomNumbers;
+  document.querySelector(".iconBottom").innerHTML = randomIcon;
+
+  if (randomIcon == "♥" || randomIcon == "♦") {
+    document.querySelector(".iconTop").style.color = "red";
+    document.querySelector(".iconBottom").style.color = "red";
+  } else {
+    document.querySelector(".iconTop").style.color = "black";
+    document.querySelector(".iconBottom").style.color = "black";
+  }
+};
